e9a1ea3006b9e150a41d239b2253201ea4f1ab62
Kurt Pantaleon <140688186+kurtpantaleon@users.noreply.github.com>
2025-03-18 19:56:56 +0800

    update again

---
+++ Structify/src/pages/Week 2 Pages/Lesson 2/L2Page1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function L2Page1() {
+  return (
+    <div>L2Page1</div>
+  )
+}
+
+export default L2Page1
---
+++ Structify/src/pages/Week 2 Pages/Lesson 3/L3Page1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function L3Page1() {
+  return (
+    <div>L3Page1</div>
+  )
+}
+
+export default L3Page1
