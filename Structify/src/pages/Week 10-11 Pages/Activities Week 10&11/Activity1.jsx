538414ada84368dc6405b9bc5088a58bf1e3452f
JhonnBretana <jhonnbretz@gmail.com>
2025-03-19 01:30:42 +0800

    MGA FOLDER NG WEEK WALA PANG ROUTING

---
+++ Structify/src/pages/Week 10-11 Pages/Activities Week 10&11/Activity1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Activity1() {
+  return (
+    <div>Activity1</div>
+  )
+}
+
+export default Activity1
---
+++ Structify/src/pages/Week 7-8 Pages/Lesson 3/L3Page1.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import SubHeading2 from '../../../components/SubHeading2';
+import Header from '../../../components/Header';
+import LessonPages from '../../../components/LessonPages';
+import Bullet1 from '../../../assets/images/Week2-3 Images/Lesson3/Page 1/Bullet 1.png';
+import Bullet2 from '../../../assets/images/Week2-3 Images/Lesson3/Page 1/Bullet 2.png';
+import BigLeftNextIcon from '../../../assets/images/Big Left Next Icon.png';
+import BigRightNextIcon from '../../../assets/images/Big Right Next Icon.png';
+import LessonFooter from '../../../components/LessonFooter';
+
+
+
+export default function L3Page1() {
+
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    navigate(path); // navigating to the given path
+  }
+
+  // Lesson content stored in an array
+  const lessons = [
+    {
+      description: <></>,
+      image: Bullet1
+    },
+    {
+      description: <></>,
+      image: Bullet2
+    }
+  ];
+
+  return (
+    <div className="min-h-screen bg-[#1F274D] text-white flex flex-col">
+      <Header />
+      <SubHeading2 />
+      <LessonPages 
+        title=""
+        lessons={lessons}  // Pass lessons array
+        leftIcon={BigLeftNextIcon}
+        rightIcon={BigRightNextIcon}
+      />
+      <LessonFooter 
+      buttonText="Continue"
+      onClick={handleClick} 
+      path="/week2L3Page2"
+      />
+    </div>
+  );
+}
+
+ 
