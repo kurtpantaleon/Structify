36878414c6c5940cb24a99d0f7896bf8c7494534
Kurt Pantaleon <140688186+kurtpantaleon@users.noreply.github.com>
2025-03-30 13:36:23 +0800

    firebaseConfigUpdated

--- Structify/src/services/firebaseConfig.js
+++ Structify/src/services/firebaseConfig.js
@@ -8,12 +8,12 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "structify-f712f.firebaseapp.com",
-  projectId: "structify-f712f",
-  storageBucket: "structify-f712f.firebasestorage.app",
-  messagingSenderId: "857880871867",
-  appId: "1:857880871867:web:bd8463912c0e97f0027a9e",
-  measurementId: "G-WY0D82R92E"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
