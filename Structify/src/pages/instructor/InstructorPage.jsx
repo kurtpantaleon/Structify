a08f7cd7bed0a0f067c544c49b6d07bb7d62058f
Kurt Pantaleon <140688186+kurtpantaleon@users.noreply.github.com>
2025-04-13 23:58:51 +0800

    fixed protectedroute and added loading circle

---
+++ Structify/src/pages/instructor/InstructorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/AdminHeader'
+import AdminSubHeading from '../../components/SubHeading'
+
+function instructorPage() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default instructorPage
