84b29926d26a6fdb337c367938f8189ff956ce33
Kurt Pantaleon <140688186+kurtpantaleon@users.noreply.github.com>
2025-04-21 00:15:51 +0800

    navigation bar in instructor fixed

--- Structify/src/pages/instructor/ViewStudentLists.jsx
+++ Structify/src/pages/instructor/ViewStudentLists.jsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../../services/firebaseConfig';
 import Header from '../../components/AdminHeader';
-import AdminNavigationBar from '../../components/AdminNavigationBar';
+import AdminNavigationBar from '../../components/InstructorNavigationBar';
 import AdminSubHeading from '../../components/AdminSubHeading';
 
 function ViewStudentLists() {
@@ -18,7 +18,7 @@ function ViewStudentLists() {
       try {
         const q = query(
           collection(db, 'users'),
-          where('section', '==', section),
+          where('section', '==', section.sectionName),
           where('role', '==', 'student')
         );
         const snapshot = await getDocs(q);
@@ -45,18 +45,9 @@ function ViewStudentLists() {
         </div>
       )}
 
-      <div className="max-w-6xl mx-auto mt-7 bg-white p-6 rounded-lg shadow h-[75vh] flex flex-col">
-        <div className="flex justify-end mb-4">
-          <button
-            onClick={() => navigate(-1)}
-            className="bg-[#141a35] text-white text-sm font-medium px-4 py-2 rounded-md hover:bg-[#1f274d] transition"
-          >
-            Back
-          </button>
-        </div>
-
+      <div className="max-w-6xl mx-auto mt-7 bg-white p-10 rounded-lg shadow h-[75vh] flex flex-col">
         <h1 className="text-3xl font-extrabold text-[#141a35] mb-4">
-          Students in {section}
+          {section.sectionName}
         </h1>
 
         <div className="flex items-center justify-between mb-3">
