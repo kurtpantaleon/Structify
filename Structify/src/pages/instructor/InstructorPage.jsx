4a3c930b813cf52f6692128af314522adc37bdf4
Kurt Pantaleon <140688186+kurtpantaleon@users.noreply.github.com>
2025-04-29 21:27:31 +0800

    fixed viewsstudent in instructor page

--- Structify/src/pages/instructor/InstructorPage.jsx
+++ Structify/src/pages/instructor/InstructorPage.jsx
@@ -71,14 +71,7 @@ function InstructorPage() {
       )}
 
       <div className="max-w-7xl mx-auto mt-6 bg-white p-6 rounded-lg shadow h-[75vh] overflow-y-auto">
-        <div className="flex justify-end mb-4">
-          <button
-            onClick={() => setShowModal(true)}
-            className="bg-[#141a35] text-white text-sm font-medium px-4 py-2 rounded-md hover:bg-[#1f274d] transition"
-          >
-            View Student Lists
-          </button>
-        </div>
+        
         <div className="grid grid-cols-1 m-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {sections.map((section) => (
             <SectionCard
