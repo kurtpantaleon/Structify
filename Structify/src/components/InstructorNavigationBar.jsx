09e65839954631be2da82610a09b8c2a48bb5651
Kurt Pantaleon <140688186+kurtpantaleon@users.noreply.github.com>
2025-05-12 08:43:07 +0800

    added forum in navbar

--- Structify/src/components/InstructorNavigationBar.jsx
+++ Structify/src/components/InstructorNavigationBar.jsx
@@ -3,15 +3,17 @@ import { useNavigate } from "react-router-dom";
 import HomeIcon from "../assets/images/Home Icon.png"; 
 import Leaderboard from "../assets/images/Leaderboard Icon.png";
 import PvPIcon from "../assets/images/PvP Icon.png";
+import ForumIcon from "../assets/images/Forum Icon.png";
 
 const InstructorNavigationBar = () => {
   return (
-    <div className="fixed bg-[#1F274D] h-screen w-22 flex flex-col items-center py-8 space-y-8 px-1 border-r-1 border-gray-200">
+    <div className="fixed bg-[#1F274D] h-screen w-22 flex flex-col items-center py-8 space-y-8 px-1 border-gray-200">
       {/* Navigation Icons */}
       <nav className="flex flex-col space-y-8">
         <NavItem to="/InstructorPage" icon={HomeIcon} label="Home" />
         <NavItem to="/Leaderboard" icon={Leaderboard} label="Leaderboard" />
         <NavItem to="" icon={PvPIcon} label="PvP" /> 
+        <NavItem to="" icon={ForumIcon} label="Forum" />
       </nav>
     </div>
   );
@@ -27,7 +29,7 @@ const NavItem = ({ to, icon, label }) => {
 
   return (
     <div className="flex flex-col items-center cursor-pointer group" onClick={handleNavigation}>
-      <img src={icon} alt={label} className="h-8 w-9 group-hover:opacity-75 transition-opacity" />
+      <img src={icon} alt={label} className="h-9 w-9 group-hover:opacity-75 transition-opacity" />
       <span className="text-white text-xs mt-1">{label}</span>
     </div>
   );
--- Structify/src/components/NavigationBar.jsx
+++ Structify/src/components/NavigationBar.jsx
@@ -4,20 +4,22 @@ import CodeIcon from "../assets/images/Code Icon.png";
 import LeaderboardIcon from "../assets/images/Leaderboard Icon.png";
 import LearnIcon from "../assets/images/Learn Icon.png";
 import PvPIcon from "../assets/images/PvP Icon.png";
+import ForumIcon from "../assets/images/Forum Icon.png";
 
 const NavigationBar = () => {
   return (
     <div
       className="fixed bottom-0 left-0 right-0 bg-[#1F274D] flex justify-around items-center py-2 px-2 border-t border-gray-200 z-50
                  sm:py-3 sm:px-4
-                 md:top-0 md:bottom-auto md:left-0 md:right-auto md:h-screen md:w-20 md:flex-col md:justify-start md:items-center md:py-8 md:space-y-8 md:border-t-0 md:border-r md:border-gray-200"
+                 md:top-0 md:bottom-auto md:left-0 md:right-auto md:h-screen md:w-22 md:flex-col md:justify-start md:items-center md:py-8 md:space-y-8 md:border-t-0 md:border-r md:border-gray-200"
     >
       {/* Navigation Icons */}
       <nav className="flex w-100 justify-around md:flex-col md:space-y-8 md:justify-start">
         <NavItem to="/mainPage" icon={LearnIcon} label="Learn" />
         <NavItem to="/codePlayground" icon={CodeIcon} label="Code" />
-        <NavItem to="/pvp" icon={PvPIcon} label="PvP" />
         <NavItem to="/Leaderboard" icon={LeaderboardIcon} label="Leaderboard" />
+        <NavItem to="/pvp" icon={PvPIcon} label="PvP" />
+        <NavItem to="/Forum" icon={ForumIcon} label="Forum" />
       </nav>
     </div>
   );
@@ -42,9 +44,9 @@ const NavItem = ({ to, icon, label }) => {
       <img
         src={icon}
         alt={label}
-        className="h-5 w-5 sm:h-6 sm:w-6 md:h-7 md:w-7 lg:h-8 lg:w-8 group-hover:opacity-75 transition-opacity"
+        className="h-8 w-9 sm:h-6 sm:w-6 md:h-7 md:w-7 lg:h-8 lg:w-9 group-hover:opacity-75 transition-opacity"
       />
-      <span className="text-white text-[9px] sm:text-[10px] mt-1 md:text-xs md:mt-2 hidden md:block">{label}</span>
+      <span className="text-white text-[8px] sm:text-[9px] mt-1 md:text-xs md:mt-2 hidden md:block">{label}</span>
     </div>
   );
 };
